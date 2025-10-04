af2856195ee5a52af7a06d76131118b5ee9db161
Ismael Celis <ismaelct@gmail.com>
2025-10-04 11:42:18 +0100

    ContentType with .parse(string) and .parse_accept(string)

---
+++ lib/steppe/content_type.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Steppe
+  class ContentType < Data.define(:type, :subtype, :params)
+
+    TOKEN = /[!#$%&'*+\-.^_`|~0-9A-Z]+/i
+    MIME_TYPE = /(?<type>#{TOKEN})\/(?<subtype>#{TOKEN})/
+    QUOTED_STRING = /"(?:\\.|[^"\\])*"/
+    PARAMETER = /\s*;\s*(?<key>#{TOKEN})=(?<value>#{TOKEN}|#{QUOTED_STRING})/
+
+    def self.parse(str)
+      m = MIME_TYPE.match(str) or
+      raise ArgumentError, "invalid content type: #{str.inspect}"
+
+      params = {}
+      str.scan(PARAMETER) do
+        key, value = Regexp.last_match.values_at(:key, :value)
+        value = value[1..-2] if value&.start_with?('"') # unquote
+        params[key.downcase] = value
+      end
+
+      new(m[:type].downcase, m[:subtype].downcase, params)
+    end
+
+    def self.parse_accept(header)
+      header.split(/\s*,\s*/).map do |entry|
+        # Match the MIME type part
+        m = MIME_TYPE.match(entry)
+        next unless m
+
+        params = {}
+        # Iterate over all parameters
+        entry.scan(PARAMETER) do |match|
+          key, value = Regexp.last_match.values_at(:key, :value)
+          # Remove quotes if quoted
+          value = value[1..-2] if value&.start_with?('"')
+          params[key.downcase] = value
+        end
+
+        new(m[:type].downcase, m[:subtype].downcase, params)
+      end.compact.sort_by { |ct| -ct.quality }
+    end
+
+    def qualified? = !(type == '*' && subtype == '*')
+
+    def to_s
+      param_str = params.map { |k, v| "#{k}=#{v}" }.join('; ')
+      [ "#{type}/#{subtype}", param_str ].reject(&:empty?).join('; ')
+    end
+
+    def ==(other)
+      other.is_a?(ContentType) &&
+        type == other.type &&
+        subtype == other.subtype &&
+        params == other.params
+    end
+
+    alias eql? ==
+    def hash = [type, subtype, params].hash
+
+    def quality = params.fetch('q', 1.0).to_f
+  end
+end
---
+++ spec/content_type_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'steppe/content_type'
+
+RSpec.describe Steppe::ContentType do
+  describe '.parse' do
+    it 'parses string into a ContentType struct' do
+      ct = described_class.parse('application/json; version=1.0')
+      expect(ct.type).to eq('application')
+      expect(ct.subtype).to eq('json')
+      expect(ct.params).to eq('version' => '1.0')
+      expect(ct.quality).to eq(1.0)
+      expect(ct.to_s).to eq('application/json; version=1.0')
+    end
+
+    it 'parses quality factor, if available' do
+      ct = described_class.parse('application/json; version=1.0; q=0.5')
+      expect(ct.quality).to eq(0.5)
+    end
+
+    specify '#==' do
+      ct1 = described_class.parse('application/json; version=1.0')
+      ct2 = described_class.parse('application/json; version=1.0')
+      ct3 = described_class.parse('application/json; version=2.0')
+
+      expect(ct1).to eq(ct2)
+      expect(ct1).not_to eq(ct3)
+    end
+  end
+
+  describe '.parse_accept' do
+    it 'parses Accept header into an array of ContentType structs, sorted by quality factor' do
+      header = 'text/html;q=0.7, application/xhtml+xml, application/xml;q=0.9, */*;q=0.6'
+      list = described_class.parse_accept(header)
+      expect(list.map(&:to_s)).to eq(['application/xhtml+xml', 'application/xml; q=0.9', 'text/html; q=0.7', '*/*; q=0.6'])
+    end
+  end
+end
